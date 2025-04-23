2a8be4db08b7d1a1bafae1380bde807be75ee4fc
brennan7mcdonald <brennan7mcdonald@gmail.com>
2025-04-23 06:44:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     const response = await fetch(`https://api.parcelsapp.com/v4/trackings/${number}`, {
       method: 'GET',
       headers: {
-        Authorization: 'Bearer YOUR_API_KEY_HERE',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiJkMWIzZTk1MC0xZjU3LTExZjAtOWNlYi0xNTI0OWNmMWIyM2UiLCJzdWJJZCI6IjY4MDc1OWY4ZmRjMDI1MmM1ZDgxYTY1YiIsImlhdCI6MTc0NTMxMjI0OH0.lSchPp4y7n3rMaAkCTGfm2d9rdAIsEaEe7IQVufFrRk',
         'Content-Type': 'application/json',
       },
     });
